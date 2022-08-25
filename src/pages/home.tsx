4ee88779192fbe6238c0c8172b115735a30056b6
Sahil Shubham <soilshubham@gmail.com>
2022-08-25 10:51:49 +0530

    Added homepage UI components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,31 @@ export const HomePage: React.FC = props => {
 	return (
 		<RootPage header='Home' cn='home'>
 			<header className='page-header'>Balance Tracker</header>
+			<main className='content'>
+				<div className='btn-group'>
+					<button>Add Address</button>
+					<button>Edit Address List</button>
+				</div>
+
+				<table className='tbl-bal'>
+					<thead>
+						<tr>
+							<th>Address</th>
+							<th>ETH</th>
+							<th>DAI</th>
+							<th>USDT</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>0x...</td>
+							<td>0.00</td>
+							<td>0.00</td>
+							<td>0.00</td>
+						</tr>
+					</tbody>
+				</table>
+			</main>
 		</RootPage>
 	)
 }
